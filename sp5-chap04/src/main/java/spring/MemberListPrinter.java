99a4f37bb6723fe39ab91cacec2d019bc6484e2a
Gayoun <makeappsgreat@gmail.com>
2022-07-08 18:22:59 +0900

    Add chap04

---
+++ sp5-chap04/src/main/java/spring/MemberListPrinter.java
@@ -0,0 +1,33 @@
+package spring;
+
+import java.util.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class MemberListPrinter {
+  
+  private MemberDao memberDao;
+  private MemberPrinter printer;
+  
+  /* public MemberListPrinter(MemberDao memberDao, MemberPrinter printer) {
+    this.memberDao = memberDao;
+    this.printer = printer;
+  } */
+  public MemberListPrinter() { }
+  
+  @Autowired
+  public void setMemberDao(MemberDao memberDao) { this.memberDao = memberDao; }
+  @Autowired
+  @Qualifier("summaryPrinter")
+  public void setMemberPrinter(MemberPrinter printer) { this.printer = printer; }
+  // public void setMemberPrinter(MemberSummaryPrinter printer) { this.printer = printer; }
+  
+  
+  public void printAll() {
+    Collection<Member> members = memberDao.selectAll();
+    
+    if (members.size() > 0) members.forEach(m -> printer.print(m));
+    else System.out.println("데이터 없음\n");
+  }
+
+}
