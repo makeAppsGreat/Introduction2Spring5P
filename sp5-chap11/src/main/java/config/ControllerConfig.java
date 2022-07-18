39a70482012c367730db3f81b7294f6d81132cd2
Gayoun <makeappsgreat@gmail.com>
2022-07-18 18:49:53 +0900

    Update chap11

--- sp5-chap11/src/main/java/config/ControllerConfig.java
+++ sp5-chap11/src/main/java/config/ControllerConfig.java
@@ -1,13 +1,27 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import controller.RegisterController;
+import spring.MemberRegisterService;
+import survey.SurveyController;
 
 @Configuration
 public class ControllerConfig {
   
+  @Autowired
+  private MemberRegisterService memberRegSvc;
+  
   @Bean
-  public RegisterController registerController() { return new RegisterController(); }
+  public RegisterController registerController() {
+    RegisterController controller = new RegisterController();
+    controller.setMemberRegisterService(memberRegSvc);
+    
+    return controller;
+  }
+  
+  @Bean
+  public SurveyController surveyController() { return new SurveyController(); }
 
 }
