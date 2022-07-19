ebfcd88a6e07be81bb6b2712bd1b8d1c643518ed
Gayoun <makeappsgreat@gmail.com>
2022-07-19 19:10:21 +0900

    Update chap11 (2)

--- sp5-chap11/src/main/java/survey/SurveyController.java
+++ sp5-chap11/src/main/java/survey/SurveyController.java
@@ -1,6 +1,9 @@
 package survey;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +13,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/survey")
 public class SurveyController {
   
+  private List<Question> createQuestions() {
+    Question q1 = new Question("당신의 역할은 무엇입니가?",
+        Arrays.asList("서버", "프론트", "풀스택"));
+    Question q2 = new Question("많이 사용하는 개발도구는 무엇입니까?",
+        Arrays.asList("이클립스", "인텔리J", "서브라임"));
+    Question q3 = new Question("하고 싶은 말은 적어주세요.");
+    
+    return Arrays.asList(q1, q2, q3);
+  }
+  
   @GetMapping
-  public String form() { return "survey/surveyForm"; }
+  public String form(Model model) {
+    List<Question> questions = createQuestions();
+    model.addAttribute("questions", questions);
+    
+    return "survey/surveyForm";
+  }
   
   @PostMapping
   public String submit(@ModelAttribute("ansData") AnsweredData data) { return "survey/submitted"; }
